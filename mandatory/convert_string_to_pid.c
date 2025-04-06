19d9de15aa9aba40834dc7e8e3f85cd0b428d5ef
Ismail El Bardi <isel-bar@c2r1p4.1337.ma>
2025-04-06 04:03:57 +0100

    chnge result 99999 to 999999

--- mandatory/convert_string_to_pid.c
+++ mandatory/convert_string_to_pid.c
@@ -6,7 +6,7 @@
 /*   By: isel-bar <isel-bar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 02:28:38 by isel-bar          #+#    #+#             */
-/*   Updated: 2025/04/05 02:29:02 by isel-bar         ###   ########.fr       */
+/*   Updated: 2025/04/06 04:01:07 by isel-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ unsigned int	convert_string_to_pid(const char *str)
 
 	result = 0;
 	i = 0;
-	while (str[i] >= '0' && str[i] <= '9' && result <= 99999)
+	while (str[i] >= '0' && str[i] <= '9' && result <= 999999)
 	{
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	if (str[i] != '\0' || result > 99999)
+	if (str[i] != '\0' || result > 999999)
 	{
 		return (0);
 	}
